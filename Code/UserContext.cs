63ebc1f828a7a1f1577d33d256b09d961463fc5f
Alexandra Zayats <41508593+Dooml3unny@users.noreply.github.com>
2018-12-19 10:41:55 +0300

    Add files via upload

---
+++ Code/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Model
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+               : base("HeapOfBooksContext")
+        { }
+
+        public DbSet<Commit> Commits { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+
+    }
+}
