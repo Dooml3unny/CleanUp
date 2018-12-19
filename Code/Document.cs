5d084161287e565cc3d332926c345acfaec01f3f
Alexandra Zayats <41508593+Dooml3unny@users.noreply.github.com>
2018-12-19 10:30:20 +0300

    Add files via upload

---
+++ Code/Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Model
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateСreation { get; set; }
+        public DateTime DateLastChange { get; set; }
+        public string FilePath { get; set; }
+
+        public int? CollectionId { get; set; }
+        public virtual Collection Collection { get; set; }
+
+        public virtual ICollection<Commit> Commits { get; set; }
+
+    }
+}
