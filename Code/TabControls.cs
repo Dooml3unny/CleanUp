e5ea01e87f6336d21cefd969e3c3c79b387a0aee
Alexandra Zayats <41508593+Dooml3unny@users.noreply.github.com>
2018-12-19 10:32:18 +0300

    Add files via upload

---
+++ Code/TabControls.cs
@@ -0,0 +1,22 @@
+﻿using CodeHeapOfBooks.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CodeHeapOfBooks.Model
+{
+    public  class TabControls
+    {
+        public string Header { get; set; }
+        public BindableBase ContentTab { get; set; }
+
+        public TabControls(string header, BindableBase contentTab)
+        {
+            this.Header = header;
+            this.ContentTab = contentTab;
+        }
+    }
+}
