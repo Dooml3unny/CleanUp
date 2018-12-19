1f9216c4705bd73198bb8aee1b8a55715698192c
Alexandra Zayats <41508593+Dooml3unny@users.noreply.github.com>
2018-12-19 10:21:46 +0300

    Add files via upload

---
+++ Code/Collection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Model
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateСreation { get; set; }
+        public virtual ICollection<Document> Documents { get; set; }
+    }
+}
