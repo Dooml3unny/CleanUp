ca152d4dfb5e5e8cf6897a94719174b91e7bb622
Alexandra Zayats <41508593+Dooml3unny@users.noreply.github.com>
2018-12-19 10:21:11 +0300

    Add files via upload
    Add code for class Book

---
+++ Code/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code.Model
+{
+   public  class Book : Document
+    {        
+        public string Author { get; set; }
+        public string Series { get; set; }
+        public int NumberInTheSeries { get; set; }
+    }
+}
